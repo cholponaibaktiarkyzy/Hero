b6db19382fad4f51a21a8b599f957194e710cede
Cholponai <cbaktiarkyzy@gmail.com>
2022-04-20 00:34:25 +0600

    Hero

---
+++ src/components/header.js
@@ -0,0 +1,19 @@
+import './Header.css';
+
+const  Header = ()=>{
+  return (
+      <header>
+        <div className="begin">
+        <ul className="point">
+          <img src="https://cdn-icons-png.flaticon.com/512/770/770706.png" />
+          <li><a href="#">Red</a></li>
+          <li><a href="#">Blue</a></li>
+          <li><a href="#">Yellow</a></li>
+          <li><a href="#">Purple</a></li>
+          <li><a href="#">Black</a></li>
+        </ul>
+        </div>
+      </header>
+  )
+}
+export default Header
